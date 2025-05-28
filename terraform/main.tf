97dcbc0022800ade9776dcbfec076098ad4e4e39
Asif Ihtemadul Haque <asifihtemadulhaque@gmail.com>
2025-05-27 21:24:34 -0400

    formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 terraform {
-  cloud { 
-    organization = "intervue" 
-    workspaces { 
+  cloud {
+    organization = "intervue"
+    workspaces {
       name = "app-database"
     }
   }
   required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
@@ -28,16 +28,16 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 resource "aws_subnet" "subnet_a" {
-  vpc_id = data.aws_vpc.default_vpc.id
-  cidr_block = cidrsubnet(data.aws_vpc.default_vpc.cidr_block, 8, 1)
+  vpc_id            = data.aws_vpc.default_vpc.id
+  cidr_block        = cidrsubnet(data.aws_vpc.default_vpc.cidr_block, 8, 1)
   availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
     Name = "${var.project_name}-db-subnet-a"
   }
 }
 resource "aws_subnet" "subnet_b" {
-  vpc_id = data.aws_vpc.default_vpc.id
-  cidr_block = cidrsubnet(data.aws_vpc.default_vpc.cidr_block, 8, 2)
+  vpc_id            = data.aws_vpc.default_vpc.id
+  cidr_block        = cidrsubnet(data.aws_vpc.default_vpc.cidr_block, 8, 2)
   availability_zone = data.aws_availability_zones.available.names[1]
   tags = {
     Name = "${var.project_name}-db-subnet-b"
@@ -51,7 +51,7 @@ resource "aws_db_subnet_group" "postgres" {
   ]
 }
 resource "aws_security_group" "db" {
-  name = "db-security-group"
+  name   = "db-security-group"
   vpc_id = data.aws_vpc.default_vpc.id
 }
 resource "aws_security_group_rule" "allow_http_inbound" {
@@ -64,16 +64,16 @@ resource "aws_security_group_rule" "allow_http_inbound" {
 }
 # PostgreSQL (RDS)
 resource "aws_db_instance" "postgres" {
-    identifier_prefix         = "${var.project_name}-postgres-"
-    instance_class            = var.postgres_instance_class
-    engine                    = "postgres"
-    allocated_storage         = 10
-    db_name                   = var.postgres_db_name
-    username                  = var.postgres_username
-    password                  = var.postgres_password
-    vpc_security_group_ids    = [aws_security_group.db.id]
-    publicly_accessible       = true
-    db_subnet_group_name      = aws_db_subnet_group.postgres.name
-    multi_az                  = true
-    skip_final_snapshot       = true
+  identifier_prefix      = "${var.project_name}-postgres-"
+  instance_class         = var.postgres_instance_class
+  engine                 = "postgres"
+  allocated_storage      = 10
+  db_name                = var.postgres_db_name
+  username               = var.postgres_username
+  password               = var.postgres_password
+  vpc_security_group_ids = [aws_security_group.db.id]
+  db_subnet_group_name   = aws_db_subnet_group.postgres.name
+  publicly_accessible    = true
+  multi_az               = true
+  skip_final_snapshot    = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,28 @@
 variable "project_name" {
-    type    = string
-    default = "intervue"
+  type    = string
+  default = "intervue"
 }
 variable "aws_region" {
-    type    = string
-    default = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 variable "aws_security_group_db_name" {
-    type = string
-    default = "intervue-db-sg"
+  type    = string
+  default = "intervue-db-sg"
 }
 variable "postgres_instance_class" {
-    type    = string
-    default = "db.t3.micro"
+  type    = string
+  default = "db.t3.micro"
 }
 variable "postgres_db_name" {
-    type = string
-    default = "appdb"
+  type    = string
+  default = "appdb"
 }
 variable "postgres_password" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 variable "postgres_username" {
-    type = string
-    default = "intervue_psql"
+  type    = string
+  default = "intervue_psql"
 }
